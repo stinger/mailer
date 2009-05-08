c2f2911dbc2889170937dc9421450af819cad037
Ilian Konchev <konchev@gmail.com>
2009-05-08 18:07:21 +0300

    Reorganized library directory

--- library/ModelsPathPlugin.php
+++
@@ -1,9 +0,0 @@
-<?php
-class ModelsPathPlugin extends Zend_Controller_Plugin_Abstract
-{
-	public function dispatchLoopStartup(Zend_Controller_Request_Abstract $request)  
-	{
-		set_include_path(get_include_path() . PATH_SEPARATOR . ROOT_DIR . '/application/modules/' . $request->getModuleName() . '/models');
-	}
-}
-?>
